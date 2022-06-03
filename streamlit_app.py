ae5529604d5bbaeba0a14e51ab6ab24e01f96446
Catriona Chan <63988025+catrionachan@users.noreply.github.com>
2022-06-03 14:43:23 -0400

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,2 +1,9 @@
 import streamlit as st
 st.write("Hello!")
+
+categories =['a', 'b', 'c']
+st.multiselect("Pick an option!", categories)
+st.button("Click me!")
+if st.checkbox("Select me!"):
+  st.write("you selected the checkbox!")
+st.write("Hello")
